3685292c3b0f04b4919c8f705e8a9b897b7f1c4d
MinHeinKoKo <kokominhein96@gmail.com>
2024-02-16 09:55:26 +0630

    making operator for level 0

--- app/src/main/java/com/example/math/ChooseOperator.kt
+++ app/src/main/java/com/example/math/ChooseOperator.kt
@@ -1,11 +1,43 @@
 package com.example.math
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class ChooseOperator : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_choose_operator)
+
+        val level0Add = findViewById<Button>(R.id.Level0Add)
+        level0Add.setOnClickListener {
+            val calInt = Intent(this, Level0::class.java)
+            calInt.putExtra("cals","+")
+            startActivity(calInt)
+        }
+
+        val level0Substract = findViewById<Button>(R.id.Level0Subtract)
+        level0Substract.setOnClickListener {
+            val calInt = Intent(this, Level0::class.java)
+            calInt.putExtra("cals","-")
+            startActivity(calInt)
+        }
+
+        val level0Multiply= findViewById<Button>(R.id.Level0Multiply)
+        level0Multiply.setOnClickListener {
+            val calInt = Intent (this,Level0::class.java)
+
+            calInt.putExtra("cals","*")
+            startActivity(calInt)
+        }
+
+        val level0Divide= findViewById<Button>(R.id.Level0Divide)
+        level0Divide.setOnClickListener {
+            val calInt = Intent (this,Level0::class.java)
+
+            calInt.putExtra("cals","/")
+            startActivity(calInt)
+        }
     }
 }
